58ebeab64cd08577c2867876f342cf4d1467d6b2
wallism <mark.wallis@mrisoftware.com>
2024-07-29 16:13:04 +1000

    add v32 full framework base that will work with editor - includes SyncService

---
+++ Full/v32/buildbasefortxcontrol-full.ps1
@@ -0,0 +1,8 @@
+# First - copy C:\Windows\Fonts (the whole folder) to this dir (then it is in context for the dockerfile, so it can copy into the image)
+# change the tags (all 4 places) to todays date
+docker build -t aspnet-with-txcontrol-dependencies-v32:4.8-20240729 .
+
+# tag with public repo prefix, so can push the update
+docker tag aspnet-with-txcontrol-dependencies-v32:4.8-20240729 wallism/aspnet-with-txcontrol-dependencies-v32:4.8-20240729
+# push to public repo (you need to login to dockerhub)
+docker push wallism/aspnet-with-txcontrol-dependencies-v32:4.8-20240729
