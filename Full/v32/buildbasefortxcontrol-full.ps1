fa372e69afffaa9f307fc3cc5313d3900e25209f
mark-wallis-apac <mark.wallis@mrisoftware.com>
2025-01-31 08:14:01 +1100

    comment out local test run command in build script

--- Full/v32/buildbasefortxcontrol-full.ps1
+++ Full/v32/buildbasefortxcontrol-full.ps1
@@ -3,7 +3,8 @@
 docker build -t aspnet-with-txcontrol-dependencies-v32:4.8-windowsservercore-ltsc2022-20250130 .
 
 # test locally
-docker run -d --rm --name v32-txtextcontrol-test lpcontainerregistry.azurecr.io/aspnet-with-txcontrol-dependencies-v32:4.8-windowsservercore-ltsc2022-20250130
+#docker run -d --rm --name v32-txtextcontrol-test lpcontainerregistry.azurecr.io/aspnet-with-txcontrol-dependencies-v32:4.8-windowsservercore-ltsc2022-20250130
+
 # tag with public repo prefix, so can push the update
 docker tag aspnet-with-txcontrol-dependencies-v32:4.8-windowsservercore-ltsc2022-20250130 wallism/aspnet-with-txcontrol-dependencies-v32:4.8-windowsservercore-ltsc2022-20250130
 # push to public repo (you need to login to dockerhub)
